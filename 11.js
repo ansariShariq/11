3f60a7747093ff2bc912aae7acc2b33ab7691aca
Niket Nayan <niketnayan13@gmail.com>
2021-06-17 19:37:02 +0530

    project files

---
+++ 11.js
@@ -0,0 +1,10 @@
+function sendmsg(){
+    var name="LINK SENT, CHECK YOUR MESSAGE BOX"
+    alert(name)
+}
+function anddown(){
+    alert("LINK FOR ANDROID DEVICE")
+}
+function macdown(){
+    alert("LINK FOR MAC IOS DEVICE")
+}
